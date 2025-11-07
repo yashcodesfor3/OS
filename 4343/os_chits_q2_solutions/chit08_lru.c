9eaef410c47e627e84c773913384d72adea00b8d
yashcodesfor3 <yashjadhav1509.work@gmail.com>
2025-11-07 07:33:51 +0530

    Update chit08_lru.c

--- 4343/os_chits_q2_solutions/chit08_lru.c
+++ 4343/os_chits_q2_solutions/chit08_lru.c
@@ -1,27 +1,60 @@
 #include <stdio.h>
-#define MAXF 10
-#define MAXP 100
+
+void printFrames(int frame[], int f){
+    for(int i=0;i<f;i++){
+        if(frame[i]==-1) printf("- ");
+        else printf("%d ",frame[i]);
+    }
+    printf("\n");
+}
 
 int main(){
-    int f,pn; printf("Frames and pages: "); if(scanf("%d%d",&f,&pn)!=2) return 0;
-    int pages[pn]; for(int i=0;i<pn;i++) scanf("%d",&pages[i]);
-    int frame[MAXF], age[MAXF]; for(int i=0;i<f;i++){ frame[i]=-1; age[i]=0; }
-    int faults=0;
-    for(int t=0;t<pn;t++){
-        int x=pages[t], hit=-1;
-        for(int i=0;i<f;i++) if(frame[i]==x){ hit=i; break; }
-        for(int i=0;i<f;i++) if(frame[i]!=-1) age[i]++;
-        if(hit!=-1){ age[hit]=0; }
-        else{
-            faults++;
-            int idx=-1, oldest=-1;
-            for(int i=0;i<f;i++){
-                if(frame[i]==-1){ idx=i; break; }
-                if(age[i]>oldest){oldest=age[i]; idx=i;}
+    int pages[50], frame[10], used[10]={0};
+    int pn, f, i, j, pos, time=0, faults=0;
+
+    printf("Enter number of pages: ");
+    scanf("%d",&pn);
+
+    printf("Enter page reference string: ");
+    for(i=0;i<pn;i++)
+        scanf("%d",&pages[i]);
+
+    printf("Enter number of frames (minimum 3): ");
+    scanf("%d",&f);
+
+    for(i=0;i<f;i++)
+        frame[i] = -1;
+
+    printf("\nLRU PAGE REPLACEMENT:\n");
+
+    for(i=0;i<pn;i++){
+        int page = pages[i];
+        int found = 0;
+
+        // check hit
+        for(j=0;j<f;j++){
+            if(frame[j] == page){
+                found = 1;
+                used[j] = time++;
+                break;
             }
-            frame[idx]=x; age[idx]=0;
         }
+
+        // if miss
+        if(found == 0){
+            pos = 0;
+            for(j=1;j<f;j++)
+                if(used[j] < used[pos]) pos=j;
+
+            frame[pos] = page;
+            used[pos] = time++;
+            faults++;
+        }
+
+        printf("Page %d -> ",page);
+        printFrames(frame,f);
     }
-    printf("LRU faults=%d\n", faults);
+
+    printf("\nTotal Page Faults = %d\n",faults);
     return 0;
 }
